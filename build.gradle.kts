e07e625dcb3221964e02905830a5c8f96b48c24f
sinhajun <sinsinhajun@gmail.com>
2023-05-25 10:13:10 +0900

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,8 +15,8 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
 }
 
 tasks.test {
