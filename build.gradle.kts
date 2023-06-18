76163967110e99fbf7c986c42a3601fab21de4f4
sinhajun <sinsinhajun@gmail.com>
2023-06-18 17:30:25 +0900

    support 1.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
     implementation(kotlin("stdlib"))
     implementation(kotlin("reflect"))
 }
